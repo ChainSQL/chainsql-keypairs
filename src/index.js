ff7036889a2985f72f6980c629c6d20509984f7f
ruancheng <cruanysu@gmail.com>
2020-09-09 16:24:58 +0800

    1 import library chainsql-sm-crypto to implement soft sm2 sm3 sm4 algorithm 2 upgrade version to 0.10.9

--- src/index.js
+++ src/index.js
@@ -11,14 +11,22 @@ const derivePrivateKey = require('./secp256k1').derivePrivateKey
 const accountPublicFromPublicGenerator = require('./secp256k1')
   .accountPublicFromPublicGenerator
 const utils = require('./utils')
+
+const sm2= require('chainsql-sm-crypto').sm2;
+const sm3 = require('chainsql-sm-crypto').sm3;
+const sm4 = require('chainsql-sm-crypto').sm4;
+
+
+
 const hexToBytes = utils.hexToBytes
 const bytesToHex = utils.bytesToHex
 
 const sdServerUrl = "http://127.0.0.1:8000";
 const ACCOUNT_PUBLIC = 35;
-var cryptAlgType = "normal";
+var cryptAlgType = "normal"; // "gmAlg"  "normal"  "softGMAlg"
 
 function generateSeed(options = {}) {
+
 	assert(!options.entropy || options.entropy.length >= 16, 'entropy too short')
 	const entropy = options.entropy ? options.entropy.slice(0, 16) : brorand(16)
 //   const type = options.algorithm === 'ed25519' ? 'ed25519' : 'secp256k1'
@@ -33,12 +41,18 @@ function generateSeed(options = {}) {
 		case "gmAlg":
 			type = "gmAlg";
 			break;
+		case "softGMAlg":
+			type = "softGMAlg";
+			break;
 		default:
 			type = "secp256k1";
 	}
 	if (type === "gmAlg") {
-		return "gmAlg"
-	} else {
+		return {type:"gmAlg"};
+	}else	if (type === "softGMAlg") {
+		var secretStr  = options.secret ? options.secret : "";
+		return {type:"softGMAlg",secret:secretStr};
+	}else{
 		return addressCodec.encodeSeed(entropy, type);
 	}
 }
@@ -66,45 +80,50 @@ const secp256k1 = {
 }
 
 const ed25519 = {
-  deriveKeypair: function(entropy) {
-    const prefix = 'ED'
-    const rawPrivateKey = hash(entropy)
-    const privateKey = prefix + bytesToHex(rawPrivateKey)
-    const publicKey = prefix + bytesToHex(
-      Ed25519.keyFromSecret(rawPrivateKey).pubBytes())
-    return {privateKey, publicKey}
-  },
-  sign: function(message, privateKey) {
-    // caution: Ed25519.sign interprets all strings as hex, stripping
-    // any non-hex characters without warning
-    assert(Array.isArray(message), 'message must be array of octets')
-    return bytesToHex(Ed25519.sign(
-      message, hexToBytes(privateKey).slice(1)).toBytes())
-  },
-  verify: function(message, signature, publicKey) {
-    return Ed25519.verify(message, hexToBytes(signature),
-      hexToBytes(publicKey).slice(1))
-  }
+	deriveKeypair: function(entropy) {
+		const prefix = 'ED'
+		const rawPrivateKey = hash(entropy)
+		const privateKey = prefix + bytesToHex(rawPrivateKey)
+		const publicKey = prefix + bytesToHex(
+		  Ed25519.keyFromSecret(rawPrivateKey).pubBytes())
+		return {privateKey, publicKey}
+	  },
+	  sign: function(message, privateKey) {
+		// caution: Ed25519.sign interprets all strings as hex, stripping
+		// any non-hex characters without warning
+		//console.log(message);
+		//assert(Array.isArray(message), 'message must be array of octets')
+		return bytesToHex(Ed25519.sign(
+		  message, hexToBytes(privateKey).slice(1)).toBytes())
+	  },
+	  verify: function(message, signature, publicKey) {
+		return Ed25519.verify(message, hexToBytes(signature),
+		  hexToBytes(publicKey).slice(1))
+	  }
 }
 
 const gmAlg = {
 	deriveKeypair: function(entropy) {
-		const genKeyUrl = sdServerUrl + "/2/21";
-		const getPubUrl = sdServerUrl + "/2/22";
-		const postData = { containerIndex:0 };
-		var privateKey = "", publicKey = "";
+
+		var genKeyUrl = sdServerUrl + "/2/21";
+		var getPubUrl = sdServerUrl + "/2/22";
+		var postData = { containerIndex: 0 };
+		var privateKey = "",
+		    publicKey = "";
 		try {
-			const retGenKey = sendPost(genKeyUrl, postData);
-			const retGetPub = sendPost(getPubUrl, postData);
+			var retGenKey = sendPost(genKeyUrl, postData);
+			var retGetPub = sendPost(getPubUrl, postData);
 			privateKey = "4700";
 			// console.log(retGetPub.retCode);
 			publicKey = retGetPub.publicKey;
 		} catch (error) {
 			throw new Error(error);
 		}
-		return {privateKey, publicKey}
+		return { privateKey: privateKey, publicKey: publicKey };
+
 	},
 	sign: function(message, privateKey) {
+
 		const sm3HashUrl = sdServerUrl + "/3";
 		const sm2SignUrl = sdServerUrl + "/2/24";
 
@@ -126,6 +145,7 @@ const gmAlg = {
 		}
 	},
 	verify: function(message, signature, publicKey) {
+
 		const sm3HashUrl = sdServerUrl + "/3";
 		const sm2VerifyUrl = sdServerUrl + "/2/25";
 		const msgHexStr = gmByte2HexStr(message);
@@ -149,6 +169,103 @@ const gmAlg = {
 	}
 }
 
+const softGMAlg = {
+
+	/**
+	 * 通过指定私钥，生成公私钥对
+	 * @param {*} secret 
+	 */
+	deriveKeypair: function(secretBytes) {
+
+		var keypair
+		if( secretBytes.length > 0){
+			var secretStr  =  utils.bytesToString(secretBytes)
+			var privateHex =  addressCodec.decodeAccountPrivate(secretStr)
+			keypair  = sm2.generateKeyPairFromSeed(privateHex)
+		}else{
+
+			keypair  = sm2.generateKeyPairHex()
+			var encodedNodePrivate =  addressCodec.encodeNodePrivate(utils.hexToBytes(keypair.privateKey));
+			console.log(encodedNodePrivate)
+		}
+		return keypair
+	},
+	sign: function(message, privateKey) {
+	
+		let signature =  sm2.doSignature(message, privateKey);
+		return signature;
+	},
+	verify: function(message, signature, publicKey) {
+
+		let bVerify = sm2.doVerifySignature(message, signature,publicKey)
+		return bVerify;
+	},
+	sm2Enc: function(plainData,publicKey) {
+
+		if(publicKey.length !== 130){
+			let decoded = addressCodec.decode(publicKey, ACCOUNT_PUBLIC);
+			let decodedPublic = decoded.slice(1, decoded.length-4);
+			publicKey = utils.bytesToHex(decodedPublic);
+		}
+
+		var cipper = sm2.doEncryptFromHex(plainData.toUpperCase(),publicKey)
+		return cipper;	
+	},
+	sm2Dec: function(priKey, cipherData) {
+
+		var privateHex = addressCodec.decodeAccountPrivate(priKey)
+		var keypair    = sm2.generateKeyPairFromSeed(privateHex)
+		return sm2.doDecrypt(cipherData,keypair.privateKey);
+
+	},
+	symEnc:function(symKey, plaintext){
+
+		var plainHex    = utils.parseUtf8StringToHex(plaintext);
+		var plainArr    = utils.hexToArray(plainHex);
+		var symKeyBytes = utils.hexToBytes(symKey)
+
+		var cipper =  sm4.sm4SymEnc(plainArr,symKeyBytes);
+
+		var ciperHex = utils.bytesToHex(cipper);
+		return ciperHex;
+	},
+	symDec:function(symKey, hexCipherData){
+
+		// sm4解密 
+		var symKeyBytes  = utils.hexToBytes(symKey)
+		var cipherArr    = utils.hexToArray(hexCipherData);	
+		var plainBytes   = sm4.sm4SymDec(cipherArr, symKeyBytes)
+		return utils.bytesToHex(plainBytes)
+
+	},
+	asymEnc:function(message, publicKey){
+
+		var plainData = Buffer.from(message, 'utf8').toString('hex');
+		if(publicKey.length !== 130){
+			let decoded = addressCodec.decode(publicKey, ACCOUNT_PUBLIC);
+			let decodedPublic = decoded.slice(1, decoded.length-4);
+			publicKey = utils.bytesToHex(decodedPublic);
+		}
+
+		var cipper = sm2.doEncryptFromHex(plainData.toUpperCase(),publicKey)
+		return cipper;	
+
+	},
+	asymDec:function(cipherDataHex, privateKey){
+
+		var privateHex =  addressCodec.decodeAccountPrivate(privateKey)
+		var keypair    =  sm2.generateKeyPairFromSeed(privateHex)
+		return sm2Test.doDecrypt(cipherDataHex,keypair.privateKey);
+	},
+	softSM3:function(hexStr){
+ 
+	    let sm3Msg = sm3.sm3FromHexString(hexStr);
+		return sm3Msg.toUpperCase();
+	}
+}
+
+
+
 function gmByte2HexStr(message) {
 	let msgHexStr;
 	let originalData = ArrayBuffer.isView(message) ? Buffer.from(message) : message;
@@ -164,15 +281,31 @@ function gmByte2HexStr(message) {
 }
 
 function select(algorithm) {
-	const methods = {"gmAlg": gmAlg, 'ecdsa-secp256k1': secp256k1, ed25519}
-  return methods[algorithm]
+	const methods = {
+		"gmAlg": gmAlg, 
+		"softGMAlg": softGMAlg, 
+		'ecdsa-secp256k1': secp256k1, 
+		'ed25519':ed25519}
+  
+		return methods[algorithm]
 }
 
 function deriveKeypair(seed, options) {
 	let decoded = {};
-	if (seed === "gmAlg") {
-		decoded.type = "gmAlg";
-	} else {
+	var regSoftGMSeed = /^[a-zA-Z1-9]{51,51}/
+
+	if (seed.type === "gmAlg") {
+	 	decoded.type = "gmAlg";
+	}
+	else if (seed.type === "softGMAlg" ) {
+		decoded.type  = "softGMAlg";
+		decoded.bytes = utils.stringToBytes(seed.secret); 
+	}
+	else if(regSoftGMSeed.test(seed)){
+		decoded.type  = "softGMAlg";
+		decoded.bytes = utils.stringToBytes(seed); 
+	}
+	else {
 		decoded = addressCodec.decodeSeed(seed);
 	}
 
@@ -187,11 +320,15 @@ function deriveKeypair(seed, options) {
 		case "gmAlg":
 			algorithm = "gmAlg";
 			break;
+		case "softGMAlg":
+			algorithm = "softGMAlg";
+			break;			
 		default:
 			algorithm = "ecdsa-secp256k1";
 	}
 	const method = select(algorithm)
 	const keypair = method.deriveKeypair(decoded.bytes, options)
+
 	const messageToVerify = hash('This test message should verify.')
 	const signature = method.sign(messageToVerify, keypair.privateKey)
 	if (method.verify(messageToVerify, signature, keypair.publicKey) !== true) {
@@ -203,13 +340,82 @@ function deriveKeypair(seed, options) {
 function getAlgorithmFromKey(key) {
   const bytes = hexToBytes(key)
   if(bytes.length === 65 || bytes.length === 2) {
-	  if(bytes[0] === 0x47)
-	  	return "gmAlg";
-  } else {
+	  if(bytes[0] === 0x47){
+		return (cryptAlgType === 'gmAlg') ? "gmAlg" :"softGMAlg";
+	  }
+	  
+  }else if(bytes.length === 32){
+	// 软国密的私钥
+	return 'softGMAlg';
+  }else {
     return (bytes.length === 33 && bytes[0] === 0xED) ?
       'ed25519' : 'ecdsa-secp256k1'
   }
 }
+
+/**
+ * 
+ * @param {*} pubKey 
+ * @return "gmAlg" "softGMAlg" 'ed25519' 'ecdsa-secp256k1'
+ */
+function getAlgFromPubKey(pubKey){
+
+
+	let decoded = addressCodec.decode(pubKey, ACCOUNT_PUBLIC);
+	let decodedPublic = decoded.slice(1, decoded.length-4);
+
+	if(decodedPublic.length === 65 || decodedPublic.length === 2) {
+		if(decodedPublic[0] === 0x47){
+		  return (cryptAlgType === 'gmAlg') ? "gmAlg" :"softGMAlg";
+		}
+		
+	}else {
+	  return (decodedPublic.length === 33 && decodedPublic[0] === 0xED) ?
+		'ed25519' : 'ecdsa-secp256k1'
+	}
+}
+
+/**
+ * 
+ * @param {*} privateKey 
+ * @return  "softGMAlg" 'ed25519' 'ecdsa-secp256k1'
+ */
+function getAlgFromPrivateKey(privateKey){
+
+	let regSoftGMSeed = /^[a-zA-Z1-9]{51,51}/
+	let decoded = {}
+
+	if(regSoftGMSeed.test(privateKey)){
+		decoded.type  = "softGMAlg";
+	}else if(privateKey === "gmAlg"){
+		decoded.type  = "gmAlg";
+	}else {
+		decoded = addressCodec.decodeSeed(privateKey);
+	}
+
+	let algorithm;
+	switch (decoded.type) {
+		case "ed25519":
+			algorithm = "ed25519";
+			break;
+		case "secp256k1":
+			algorithm = "ecdsa-secp256k1";
+			break;
+		case "gmAlg":
+			algorithm = "gmAlg";
+			break;	
+		case "softGMAlg":
+			algorithm = "softGMAlg";
+			break;			
+		default:
+			algorithm = "ecdsa-secp256k1";
+	}
+
+	return algorithm;
+
+}
+
+
 
 function sign(messageHex, privateKey) {
   const algorithm = getAlgorithmFromKey(privateKey)
@@ -217,8 +423,16 @@ function sign(messageHex, privateKey) {
 }
 
 function signBytes(message, privateKey) {
-  const algorithm = getAlgorithmFromKey(privateKey)
-  return select(algorithm).sign(message, privateKey)
+
+	var algorithm = "ecdsa-secp256k1";
+	if(cryptAlgType === "gmAlg"){
+		algorithm = "gmAlg";
+	}else if(cryptAlgType === "softGMAlg"){
+		algorithm = "softGMAlg";
+	}else if(cryptAlgType === "ed25519"){
+		algorithm = "ed25519";
+	}
+  	return select(algorithm).sign(message, privateKey)
 }
 
 function verify(messageHex, signature, publicKey) {
@@ -253,7 +467,11 @@ function sendPost(keyUrl, postMsg) {
     return {}
 }
 
+
+
 function gmAlgSm2Enc(keyIn, plainData) {
+
+
 	let sm2EncUrl = sdServerUrl;
 	let postMessage = {
 		plainData: plainData,
@@ -279,6 +497,16 @@ function gmAlgSm2Enc(keyIn, plainData) {
 }
 
 function gmAlgSm2Dec(priKey, cipherData) {
+
+		
+	if(softGMAlg){
+
+		//TODO 
+		const cipherMode = 1; // 1 - C1C3C2，0 - C1C2C3，默认为1
+		let plainData = sm2.doDecrypt(cipherData, priKey, cipherMode); // 加密结果
+		return plainData;
+	}
+
 	const sm2DecUrl = sdServerUrl + "/2/27";
 	const postMessage = {
 		cipherData: cipherData,
@@ -324,7 +552,9 @@ function gmAlgSymDec(symKey, cipherData) {
 	}
 }
 
+
 function gmAlgSm3(message) {
+	
 	const sm3HashUrl = sdServerUrl + "/3";
 	const msgHexStr = gmByte2HexStr(message);
 	const postMessage = {
@@ -339,6 +569,38 @@ function gmAlgSm3(message) {
 	}
 }
 
+/**
+ * 非对称加密
+ * @param {*} message 
+ * @param {*} publicKey 
+ */
+function asymEncrypt(message, publicKey) {
+
+	//"gmAlg" "softGMAlg" 'ed25519' 'ecdsa-secp256k1'  
+	let algorithm = getAlgFromPubKey(publicKey)
+
+	if(algorithm !== "softGMAlg"){
+		throw new Error("gmAlg ed25519 ecdsa-secp256k1, Not currently supported");
+	}
+
+	return select(algorithm).asymEnc(message, publicKey)
+}
+
+/**
+ *  * 非对称解密
+ * @param {*} messageHex 
+ * @param {*} privateKey 
+ */
+function asymDecrypt(messageHex, privateKey) {
+
+	let algorithm = getAlgFromPrivateKey(privateKey)
+	if(algorithm !== "softGMAlg"){
+		throw new Error("gmAlg ed25519 ecdsa-secp256k1, Not currently supported")
+	}
+	return select(algorithm).asymDec(messageHex, privateKey)
+}
+
+
 module.exports = {
   generateSeed,
   deriveKeypair,
@@ -349,9 +611,19 @@ module.exports = {
   deriveNodeAddress,
   setCryptAlgType,
   getCryptAlgType,
+  getAlgFromPubKey,
+  getAlgFromPrivateKey,
   gmAlgSm2Enc,
   gmAlgSm2Dec,
   gmAlgSymEnc,
   gmAlgSymDec,
-  gmAlgSm3
+  gmAlgSm3,
+  asymEncrypt,
+  asymDecrypt,
+  softGMAlgSm2Enc:softGMAlg.sm2Enc,
+  softGMAlgSm2Dec:softGMAlg.sm2Dec,
+  softGMAlgSymEnc:softGMAlg.symEnc,
+  softGMAlgSymDec:softGMAlg.symDec,
+  softGMAlgSm3   :softGMAlg.softSM3,
+
 }
--- src/utils.js
+++ src/utils.js
@@ -1,34 +1,201 @@
-'use strict' // eslint-disable-line strict
+'use strict'; // eslint-disable-line strict
 
-const assert = require('assert')
-const hashjs = require('hash.js')
-const BN = require('bn.js')
+var assert = require('assert');
+var hashjs = require('hash.js');
+var BN = require('bn.js');
 
 function bytesToHex(a) {
-  return a.map(function(byteValue) {
-    const hex = byteValue.toString(16).toUpperCase()
-    return hex.length > 1 ? hex : '0' + hex
-  }).join('')
+  return a.map(function (byteValue) {
+    var hex = byteValue.toString(16).toUpperCase();
+    return hex.length > 1 ? hex : '0' + hex;
+  }).join('');
 }
 
+
+function stringToBytes(str){
+  var arr = [];
+  for (var i = 0, j = str.length; i < j; ++i) {
+    arr.push(str.charCodeAt(i));
+  }
+ 
+  var tmpUint8Array = new Uint8Array(arr);
+  return tmpUint8Array
+}
+
+function bytesToString(fileData){
+  var dataString = "";
+  for (var i = 0; i < fileData.length; i++) {
+    dataString += String.fromCharCode(fileData[i]);
+  }
+  return dataString
+}
+
+
 function hexToBytes(a) {
-  assert(a.length % 2 === 0)
-  return (new BN(a, 16)).toArray(null, a.length / 2)
+  assert(a.length % 2 === 0);
+  return new BN(a, 16).toArray(null, a.length / 2);
 }
 
 function computePublicKeyHash(publicKeyBytes) {
-  const hash256 = hashjs.sha256().update(publicKeyBytes).digest()
-  const hash160 = hashjs.ripemd160().update(hash256).digest()
-  return hash160
+  var hash256 = hashjs.sha256().update(publicKeyBytes).digest();
+  var hash160 = hashjs.ripemd160().update(hash256).digest();
+  return hash160;
 }
 
 function seedFromPhrase(phrase) {
-  return hashjs.sha512().update(phrase).digest().slice(0, 16)
+  return hashjs.sha512().update(phrase).digest().slice(0, 16);
 }
 
-module.exports = {
-  bytesToHex,
-  hexToBytes,
-  computePublicKeyHash,
-  seedFromPhrase
+
+// function hex2ASCII(hexx) {
+//   var hex = hexx.toString();//force conversion
+//   var str = '';
+//   for (var i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
+//       str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+//   return str;
+// }
+
+
+
+/**
+ * 解析utf8字符串到16进制
+ */
+function parseUtf8StringToHex(input) {
+  input = unescape(encodeURIComponent(input))
+
+  const length = input.length
+
+  // 转换到字数组
+  const words = []
+  for (let i = 0; i < length; i++) {
+    words[i >>> 2] |= (input.charCodeAt(i) & 0xff) << (24 - (i % 4) * 8)
+  }
+
+  // 转换到16进制
+  const hexChars = []
+  for (let i = 0; i < length; i++) {
+    const bite = (words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff
+    hexChars.push((bite >>> 4).toString(16))
+    hexChars.push((bite & 0x0f).toString(16))
+  }
+
+  return hexChars.join('')
 }
+
+/**
+ * 解析arrayBuffer到16进制字符串
+ */
+function parseArrayBufferToHex(input) {
+  return Array.prototype.map.call(new Uint8Array(input), x => ('00' + x.toString(16)).slice(-2)).join('')
+}
+
+
+
+
+/**
+ * 转成16进制串
+ */
+function arrayToHex(arr) {
+  const words = []
+  let j = 0
+  for (let i = 0; i < arr.length * 2; i += 2) {
+    words[i >>> 3] |= parseInt(arr[j], 10) << (24 - (i % 8) * 4)
+    j++
+  }
+
+  // 转换到16进制
+  const hexChars = []
+  for (let i = 0; i < arr.length; i++) {
+    const bite = (words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff
+    hexChars.push((bite >>> 4).toString(16))
+    hexChars.push((bite & 0x0f).toString(16))
+  }
+
+  return hexChars.join('')
+}
+
+/**
+ * 转成utf8串
+ */
+function arrayToUtf8(arr) {
+  const words = []
+  let j = 0
+  for (let i = 0; i < arr.length * 2; i += 2) {
+    words[i >>> 3] |= parseInt(arr[j], 10) << (24 - (i % 8) * 4)
+    j++
+  }
+
+  try {
+    const latin1Chars = []
+
+    for (let i = 0; i < arr.length; i++) {
+      const bite = (words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff
+      latin1Chars.push(String.fromCharCode(bite))
+    }
+
+    return decodeURIComponent(escape(latin1Chars.join('')))
+  } catch (e) {
+    throw new Error('Malformed UTF-8 data')
+  }
+}
+
+/**
+ * 转成ascii码数组
+ */
+function hexToArray(hexStr) {
+  const words = []
+  let hexStrLength = hexStr.length
+
+  if (hexStrLength % 2 !== 0) {
+    hexStr = leftPad(hexStr, hexStrLength + 1)
+  }
+
+  hexStrLength = hexStr.length
+
+  for (let i = 0; i < hexStrLength; i += 2) {
+    words.push(parseInt(hexStr.substr(i, 2), 16))
+  }
+  return words
+}
+
+
+/**
+ * 解析utf8字符串到16进制
+ */
+function parseUtf8StringToHex(input) 
+{
+
+    input = unescape(encodeURIComponent(input))
+    
+    const length = input.length
+    
+    // 转换到字数组
+    const words = []
+    for (let i = 0; i < length; i++) {
+      words[i >>> 2] |= (input.charCodeAt(i) & 0xff) << (24 - (i % 4) * 8)
+    }
+    
+    // 转换到16进制
+    const hexChars = []
+    for (let i = 0; i < length; i++) {
+      const bite = (words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff
+      hexChars.push((bite >>> 4).toString(16))
+      hexChars.push((bite & 0x0f).toString(16))
+    }
+    
+    return hexChars.join('')
+}
+
+
+module.exports = {
+  bytesToHex: bytesToHex,
+  hexToBytes: hexToBytes,
+  computePublicKeyHash: computePublicKeyHash,
+  seedFromPhrase: seedFromPhrase,
+  stringToBytes: stringToBytes,
+  bytesToString: bytesToString,
+  arrayToHex: arrayToHex,
+  hexToArray: hexToArray,
+  arrayToUtf8: arrayToUtf8,
+  parseUtf8StringToHex: parseUtf8StringToHex,
+};
